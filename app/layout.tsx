8f9e0838b3b96a3ac831177db59667f5a660ee7f
Narendra Singh Rathore <mrperfectth7@gmail.com>
2024-09-22 22:53:22 +0530

    Update: Logos & footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,10 +11,10 @@ const poppins = Poppins({
 })
 
 export const metadata: Metadata = {
-  title: 'Evently',
-  description: 'Evently is a platform for event management.',
+  title: 'Codement',
+  description: 'Environment for Coders',
   icons: {
-    icon: '/assets/images/logo.svg'
+    icon: '/assets/images/codement.svg'
   }
 }
 
